03e882e8cd6ae437c6a317bd18b33ee49cbad707
Paul Michael Reilly <pmr@pajato.com>
2018-06-21 09:11:00 -0400

    Lecture 12: Emoji Art demo and UITextField
    Rationale:
    =========
    
    This commit provides an extensive demo using UITableView and UICollectionView and introduces UITextView.
    
    File changes:
    ============
    
    modified:   EmojiArt.xcodeproj/project.pbxproj
    modified:   EmojiArt/Base.lproj/Main.storyboard
    modified:   EmojiArt/EmojiArtDocumentTableViewController.swift
    new file:   EmojiArt/EmojiArtView+Gestures.swift
    modified:   EmojiArt/EmojiArtView.swift
    modified:   EmojiArt/EmojiArtViewController.swift
    new file:   EmojiArt/EmojiCollectionViewCell.swift

--- EmojiArt/EmojiArtDocumentTableViewController.swift
+++ EmojiArt/EmojiArtDocumentTableViewController.swift
@@ -36,7 +36,14 @@ class EmojiArtDocumentTableViewController: UITableViewController
         emojiArtDocuments += ["Untitled".madeUnique(withRespectTo: emojiArtDocuments)]
         tableView.reloadData()
     }
-    
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        if splitViewController?.preferredDisplayMode != .primaryOverlay {
+            splitViewController?.preferredDisplayMode = .primaryOverlay
+        }
+    }
+
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
---
+++ EmojiArt/EmojiCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by Paul Michael Reilly on 6/19/18.
+//  Copyright © 2018 Paul Michael Reilly. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell
+{
+    @IBOutlet weak var label: UILabel!
+}
